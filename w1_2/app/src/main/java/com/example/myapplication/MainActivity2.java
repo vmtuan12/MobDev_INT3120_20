5c5bf3b3b0d8cda346ca6ad418ad8ebfb0504749
vmtuan12 <minhtuan3154@gmail.com>
2023-09-08 22:55:14 +0700

    homework week 1 slide 3

---
+++ w1_2/app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -0,0 +1,30 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        button = (Button) findViewById(R.id.btn1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity2.this, MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
